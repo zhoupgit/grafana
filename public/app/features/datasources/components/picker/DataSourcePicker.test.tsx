924163432e1a71c3cdab03d7ed933a1263770485
Jack Westbrook <jack.westbrook@gmail.com>
2024-03-06 14:40:55 +0100

    chore(frontend): clean up broken types imports

--- public/app/features/datasources/components/picker/DataSourcePicker.test.tsx
+++ public/app/features/datasources/components/picker/DataSourcePicker.test.tsx
@@ -1,6 +1,6 @@
 import { findByText, render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
-import { UserEvent } from '@testing-library/user-event/dist/types/setup/setup';
+import userEvent, { UserEvent } from '@testing-library/user-event';
+import * as React from 'react';
 
 import {
   DataSourceInstanceSettings,
