927680da66bdc2269c4b97322f3ca43584f8c1fe
Ashley Harrison <ashley.harrison@grafana.com>
2024-04-18 14:18:16 +0100

    create example story

--- packages/grafana-ui/src/components/EmptyState/EmptyState.story.tsx
+++ packages/grafana-ui/src/components/EmptyState/EmptyState.story.tsx
@@ -49,4 +49,26 @@ Basic.args = {
   variant: 'call-to-action',
 };
 
+export const MaxWidth: StoryFn<typeof EmptyState> = (args) => {
+  const props = {
+    button: (
+      <Button icon="plus" size="lg">
+        Create dashboard
+      </Button>
+    ),
+    message: "You haven't created any dashboards yet",
+    variant: 'call-to-action' as const,
+    children:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+  };
+  return <EmptyState {...props} />;
+};
+
+MaxWidth.args = {
+  button: 'Create',
+  children: 'Use this space to add any additional information',
+  message: "You haven't created any dashboards yet",
+  variant: 'call-to-action',
+};
+
 export default meta;
