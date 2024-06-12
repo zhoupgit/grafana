18174ecf60e4664c470918732ce4392c0e349f32
Erik Sundell <erik.sundell87@gmail.com>
2024-06-12 10:16:27 +0200

    add separate registries

---
+++ public/app/features/plugins/extensions/registry/AddedComponentRegistry.ts
@@ -0,0 +1,56 @@
+import { PluginExtensionComponentConfig } from '@grafana/data';
+
+import { PluginPreloadResult } from '../../pluginPreloader';
+
+import { Registry } from './Registry';
+
+export type AddedComponent = PluginExtensionComponentConfig & {
+  pluginId: string;
+};
+
+type RegistryType = {
+  [target: string]: AddedComponent[];
+};
+
+export class AddedComponentRegistry extends Registry<RegistryType> {
+  constructor(initialState: RegistryType = {}) {
+    super({
+      initialState,
+    });
+  }
+
+  mapToRegistry(registry: RegistryType, item: PluginPreloadResult): RegistryType {
+    const { pluginId, addedComponents, error } = item;
+
+    if (error) {
+      console.log({
+        message: 'Plugin failed to load, skip adding its components to targets.',
+        pluginId,
+        error,
+      });
+      return registry;
+    }
+
+    if (!addedComponents) {
+      return registry;
+    }
+
+    for (const config of addedComponents) {
+      const { extensionPointId } = config;
+
+      // check if config is valid, skip and warn if invalid.
+      // if(isConfigValid(config)) { ... }
+
+      if (!Array.isArray(registry[extensionPointId])) {
+        registry[extensionPointId] = [];
+      }
+
+      registry[extensionPointId].push({
+        pluginId,
+        ...config,
+      });
+    }
+
+    return registry;
+  }
+}
---
+++ public/app/features/plugins/extensions/registry/AddedLinkRegistry.ts
@@ -0,0 +1,56 @@
+import { PluginExtensionLinkConfig } from '@grafana/data';
+
+import { PluginPreloadResult } from '../../pluginPreloader';
+
+import { Registry } from './Registry';
+
+export type AddedLink = PluginExtensionLinkConfig & {
+  pluginId: string;
+};
+
+type RegistryType = {
+  [target: string]: AddedLink[];
+};
+
+export class AddedLinkRegistry extends Registry<RegistryType> {
+  constructor(initialState: RegistryType = {}) {
+    super({
+      initialState,
+    });
+  }
+
+  mapToRegistry(registry: RegistryType, item: PluginPreloadResult): RegistryType {
+    const { pluginId, addedLinks, error } = item;
+
+    if (error) {
+      console.log({
+        message: 'Plugin failed to load, skip adding its links to targets.',
+        pluginId,
+        error,
+      });
+      return registry;
+    }
+
+    if (!addedLinks) {
+      return registry;
+    }
+
+    for (const config of addedLinks) {
+      const { extensionPointId } = config;
+
+      // check if config is valid, skip and warn if invalid.
+      // if(isConfigValid(config)) { ... }
+
+      if (!Array.isArray(registry[extensionPointId])) {
+        registry[extensionPointId] = [];
+      }
+
+      registry[extensionPointId].push({
+        pluginId,
+        ...config,
+      });
+    }
+
+    return registry;
+  }
+}
