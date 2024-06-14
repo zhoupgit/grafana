51aa05037f10d99a4ae5667de672c181defd03a5
Karl Persson <kalle.persson@grafana.com>
2024-06-14 10:56:37 +0200

    Register zanana as a dskit module

--- pkg/services/authz/service.go
+++ pkg/services/authz/service.go
@@ -5,12 +5,12 @@ import (
 	"errors"
 	"fmt"
 
-	"github.com/fullstorydev/grpchan/inprocgrpc"
 	"github.com/grafana/dskit/services"
 	openfgav1 "github.com/openfga/api/proto/openfga/v1"
+	"github.com/prometheus/client_golang/prometheus"
 
 	"github.com/grafana/grafana/pkg/infra/log"
-	"github.com/grafana/grafana/pkg/registry"
+	"github.com/grafana/grafana/pkg/infra/tracing"
 	"github.com/grafana/grafana/pkg/services/authz/zanzana"
 	"github.com/grafana/grafana/pkg/services/featuremgmt"
 	"github.com/grafana/grafana/pkg/services/grpcserver"
@@ -19,21 +19,18 @@ import (
 
 type Service interface {
 	services.NamedService
-	registry.BackgroundService
-	registry.CanBeDisabled
 }
 
 var _ Service = (*Zanzana)(nil)
 
-func ProvideService(cfg *setting.Cfg, features featuremgmt.FeatureToggles, grpc grpcserver.Provider) (*Zanzana, error) {
+func ProvideZanzanaService(cfg *setting.Cfg, features featuremgmt.FeatureToggles) (*Zanzana, error) {
 	s := &Zanzana{
 		cfg:      cfg,
-		grpc:     grpc,
 		features: features,
 		logger:   log.New("zanzana"),
 	}
 	// We need to use dskit service for when we are ready to use this as a standalone module
-	s.BasicService = services.NewBasicService(s.start, s.running, nil).WithName("zanzana")
+	s.BasicService = services.NewBasicService(s.start, s.running, s.stopping).WithName("zanzana")
 
 	return s, nil
 }
@@ -44,49 +41,59 @@ type Zanzana struct {
 	cfg *setting.Cfg
 
 	logger   log.Logger
-	grpc     grpcserver.Provider
+	handle   grpcserver.Provider
 	features featuremgmt.FeatureToggles
 }
 
-func (z *Zanzana) Run(ctx context.Context) error {
-	fmt.Println("RUN")
-	if err := z.start(ctx); err != nil {
-		return err
-	}
-	return z.running(ctx)
-}
-
 func (z *Zanzana) start(ctx context.Context) error {
+	z.logger.Info("Starting zanana")
+
 	srv, err := zanzana.New(zanzana.NewStore())
 	if err != nil {
 		return fmt.Errorf("failed to start zanana: %w", err)
 	}
 
-	if z.cfg.Zanzana.Mode == setting.ZanzanaModeEmbedded {
-		// TODO we need to provide channel for when clients are created
-		channel := &inprocgrpc.Channel{}
-		openfgav1.RegisterOpenFGAServiceServer(channel, srv)
-	} else {
-		if z.grpc.IsDisabled() {
-			return errors.New("failed to start zanzana: standalone zanana needs to have grpcServer feature flag enabled")
-		}
-		openfgav1.RegisterOpenFGAServiceServer(z.grpc.GetServer(), srv)
+	tracingCfg, err := tracing.ProvideTracingConfig(z.cfg)
+	if err != nil {
+		return err
+	}
+	tracingCfg.ServiceName = "zanzana"
+
+	tracer, err := tracing.ProvideService(tracingCfg)
+	if err != nil {
+		return err
 	}
 
-	z.logger.Info("Starting zanana")
+	// authenticator interceptors.Authenticator
+	z.handle, err = grpcserver.ProvideService(z.cfg, z.features, noopAuthenticator{}, tracer, prometheus.DefaultRegisterer)
+	if err != nil {
+		return fmt.Errorf("failed to create zanzana grpc server: %w", err)
+	}
+
+	openfgav1.RegisterOpenFGAServiceServer(z.handle.GetServer(), srv)
+	grpcserver.ProvideReflectionService(z.cfg, z.handle)
 
 	return nil
 }
 
 func (z *Zanzana) running(ctx context.Context) error {
 	z.logger.Info("Running zanana")
-	select {
-	case <-ctx.Done():
+	// handle.Run is blocking so we can just run it here
+	return z.handle.Run(ctx)
+}
+
+func (z *Zanzana) stopping(err error) error {
+	if err != nil && !errors.Is(err, context.Canceled) {
+		z.logger.Error("Stopping zanzana due to unexpected error", "err", err)
+	} else {
 		z.logger.Info("Stopping zanana")
 	}
 	return nil
 }
 
-func (z *Zanzana) IsDisabled() bool {
-	return !z.features.IsEnabledGlobally(featuremgmt.FlagZanzana)
+type noopAuthenticator struct{}
+
+// for now don't perform any authentication
+func (n noopAuthenticator) Authenticate(ctx context.Context) (context.Context, error) {
+	return ctx, nil
 }
