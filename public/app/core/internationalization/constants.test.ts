9125aab9b5ade498da13a2473b3934afc629b89a
Jack Westbrook <jack.westbrook@gmail.com>
2024-08-08 12:26:28 +0200

    test(internationalisation): move files around so we can mock any use of import.meta.glob

--- public/app/core/internationalization/constants.test.ts
+++ public/app/core/internationalization/constants.test.ts
@@ -2,6 +2,10 @@ import { uniqBy } from 'lodash';
 
 import { LANGUAGES, VALID_LANGUAGES } from './constants';
 
+jest.mock('./extensions.ts', () => {
+  return {};
+});
+
 describe('internationalization constants', () => {
   it('should not have duplicate languages codes', () => {
     const uniqLocales = uniqBy(LANGUAGES, (v) => v.code);
